5231c85accf7a5051430c32b2176d2c61e9aef6e
Yannick Andres <yannick.andres@outlook.com>
2023-03-30 10:05:50 +0200

    fix: user csnnot register one card twice

--- src/App.vue
+++ src/App.vue
@@ -97,6 +97,12 @@ export default {
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
       if (userSelection.value[0]) {
+        if (
+          userSelection.value[0].position === payload.position &&
+          userSelection.value[0].faceValue === payload.faceValue
+        ) {
+          return;
+        }
         userSelection.value[1] = payload;
       } else {
         userSelection.value[0] = payload;
