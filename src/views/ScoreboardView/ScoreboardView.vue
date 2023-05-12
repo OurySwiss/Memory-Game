8bb1d460b729a640c9533a7635e40bfb464ef76b
Yannick Andres <yannick.andres@swisscom.com>
2023-05-12 08:29:06 +0200

    fix: ranking system is working but not like i want it to

--- src/views/ScoreboardView/ScoreboardView.vue
+++ src/views/ScoreboardView/ScoreboardView.vue
@@ -12,13 +12,7 @@
           </thead>
           <tbody>
             <tr v-for="(score, index) in getSortedScores()" :key="index">
-              <td class="rank">
-                {{
-                  scores[index - 1] && scores[index - 1].score === score.score
-                    ? index
-                    : index + 1
-                }}.
-              </td>
+              <td class="rank">{{ index + 1 }}.</td>
               <td class="user">{{ score.userName }}</td>
               <td class="points">{{ score.score }}</td>
             </tr>
