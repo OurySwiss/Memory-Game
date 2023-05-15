9b96ab3da1eb7ebb94b2966777857068dd5870cb
Yannick Andres <yannick.andres@swisscom.com>
2023-05-15 08:06:56 +0200

    feat: features almost finished in memoryGame.vue

--- src/views/MemoryGameView/MemoryGameView.vue
+++ src/views/MemoryGameView/MemoryGameView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h2>Maximale Versuche: {{ maxAttempts }}</h2>
     <section class="game-board">
       <card
         v-for="(card, index) in cardList"
@@ -10,6 +11,31 @@
         @select-card="flipCard"
       />
     </section>
+    <!-- Neues Modal -->
+    <Transition name="start-Game-Modal">
+      <div v-if="showStartModal" class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <div class="modal-header">
+              <h1>Willkommen zum Spiel!</h1>
+            </div>
+            <div class="modal-body">
+              <p>Wähle einen Schwierigkeitsgrad:</p>
+              <button class="submit-button" @click="setDifficulty('easy')">
+                Leicht
+              </button>
+              <button class="submit-button" @click="setDifficulty('medium')">
+                Mittel
+              </button>
+              <button class="submit-button" @click="setDifficulty('hard')">
+                Schwer
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+    <!-- Vorhandenes Modal -->
     <Transition name="submit-Score-Modal">
       <div v-if="uncovered === 8" class="modal-mask">
         <div class="modal-wrapper">
@@ -40,8 +66,31 @@
         </div>
       </div>
     </Transition>
+    <!-- Game Over Modal -->
+    <Transition name="game-Over-Modal">
+      <div v-if="showGameOverModal" class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <div class="modal-header">
+              <h1>Spiel vorbei!</h1>
+            </div>
+            <div class="modal-body">
+              <p>Du hast das Spiel verloren.</p>
+              <button class="submit-button" @click="goToGame()">
+                Neues Spiel
+              </button>
+              <button class="submit-button" @click="goToScoreboard()">
+                Scoreboard
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+
     <h2 class="status">{{ status }}</h2>
     <h2>Versuche: {{ score }}</h2>
+    <h2>Verbleibende Zeit: {{ remainingTime }} Sekunden</h2>
   </div>
 </template>
 <script src="@/views/MemoryGameView/MemoryGameView"></script>
